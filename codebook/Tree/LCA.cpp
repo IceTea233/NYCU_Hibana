5f6693f9bb5b54119d0056c72ffc7397f953c478
IceTea233 <evan85910907@gmail.com>
2022-09-29 11:27:26 +0800

    style: change to 2 space indentation

--- codebook/Tree/LCA.cpp
+++ codebook/Tree/LCA.cpp
@@ -10,25 +10,25 @@ int anc[maxn][bits] = {};
 
 int id = 0;
 void build(int x, int p) { // 0 is the fake parent of root
-    in[x] = ++id;
-    anc[x][0] = p;
-    for (int i=0; anc[x][i]; i++)
-        anc[x][i+1] = anc[ anc[x][i] ][i];
-    for (auto & y : G[x]) {
-        if (y == p)
-            continue;
-        build(y, x);
-    }
+  in[x] = ++id;
+  anc[x][0] = p;
+  for (int i = 0; anc[x][i]; i++)
+    anc[x][i + 1] = anc[anc[x][i]][i];
+  for (auto &y : G[x]) {
+    if (y == p)
+      continue;
+    build(y, x);
+  }
 }
 
 int lca(int a, int b) {
-    if (a == b)
-        return a;
-    if (in[a] > in[b])
-        swap(a, b);
-    for (int i=bits-1; i>=0; i--) {
-        if (in[a] < in[anc[b][i]])
-            b = anc[b][i];
-    }
-    return anc[b][0];
+  if (a == b)
+    return a;
+  if (in[a] > in[b])
+    swap(a, b);
+  for (int i = bits - 1; i >= 0; i--) {
+    if (in[a] < in[anc[b][i]])
+      b = anc[b][i];
+  }
+  return anc[b][0];
 }
