d137346818ad77f1d5196081ec15555c1a76f13a
shaun-0124 <114806926+shaun-0124@users.noreply.github.com>
2022-10-01 20:50:31 +0800

    Add files via upload

---
+++ codebook/Data Structures/Li Chao Tree.cpp
@@ -0,0 +1,38 @@
+// insert line, point query max. of lines
+// note that m, k of nodes are NOT initialized
+
+struct segment {
+	double m, k;
+	double val(double x) {
+		return m * x + k;
+	}
+}tr[MAXC * 4];
+
+void insert(int lb, int rb, int idx, segment seg) {
+	if (lb == rb) {
+		if(seg.val(lb) > tr[idx].val(lb))
+			tr[idx] = seg;
+ 		return;
+ 	}
+ 	int mid = (lb + rb) / 2;
+ 	if (tr[idx].m > seg.m)
+ 		swap(tr[idx],seg);
+ 	if (tr[idx].val(mid) < seg.val(mid)) {
+ 		swap(tr[idx],seg);
+ 		insert(lb, mid, idx * 2, seg);
+ 	}
+ 	else
+ 		insert(mid + 1, rb, idx * 2 + 1, seg);
+}
+
+double query(int lb, int rb, int idx, int x) {
+	if (lb == rb) return tr[idx].val(x);
+	int mid = (lb + rb) / 2;
+	if (x <= mid)
+		return max(query(lb, mid, idx * 2, x), tr[idx].val(x));
+	return max(query(mid + 1, rb, idx * 2 + 1, x), tr[idx].val(x));
+}
+
+// for [1, n]:
+// call insert(1, n, 1, seg) to insert a line seg
+// call query(1, n, 1, x) to query max. of lines at point x
